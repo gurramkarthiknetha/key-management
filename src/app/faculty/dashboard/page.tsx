87d580beacdcb60c1db3c9bd42e3ba024f29c9e0
gurramkarthiknetha <gurramkarthik2006@gmail.com>
2025-07-19 00:29:08 +0530

    clean

--- src/app/faculty/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function dashboard() {
-  return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-gray-900">Faculty Dashboard</h1>
-    </div>
-  );
-}
